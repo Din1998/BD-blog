40ca1ad5c369e0186e0a1502b3eb889c9e3c112d
din1998 <dinislam8803@gmail.com>
2023-02-21 11:33:12 +0600

    add api

--- src/App.js
+++ src/App.js
@@ -8,35 +8,36 @@ import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
 import axios from "axios";
 
 function App() {
+  const user = true;
 
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const getUser = () => {
-      fetch("https://server-1998.vercel.app/auth/login/success", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
-        })
-        .then((resObject) => {
-          setUser(resObject.user);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    getUser();
-  }, []);
+  // useEffect(() => {
+  //   const getUser = () => {
+  //     fetch("https://server-1998.vercel.app/auth/login/success", {
+  //       method: "GET",
+  //       credentials: "include",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //         "Access-Control-Allow-Credentials": true,
+  //       },
+  //     })
+  //       .then((response) => {
+  //         if (response.status === 200) return response.json();
+  //         throw new Error("authentication has been failed!");
+  //       })
+  //       .then((resObject) => {
+  //         setUser(resObject.user);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   };
+  //   getUser();
+  // }, []);
 
-  console.log(user)
+  // console.log(user)
 
   const [blog,setBlog] = useState([])
 
--- src/page/Post.js
+++ src/page/Post.js
@@ -1,18 +1,29 @@
 import { useLocation } from "react-router-dom"
+import {useEffect,useState} from'react';
 import { posts } from "../data"
 
 export default function Post({blog}) {
+  const [blog,setBlog] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://server-1998.vercel.app/api`)
+    .then(res => {
+      setBlog(res.data)
+    }).catch(err => console.log(err))
+  },[])
+
+
   const location = useLocation();
   const path = location.pathname.split('/')[0];
-  const post = blog.find((p) => p._id.toString() === path)
+  const blogs = blog.find((p) => p._id.toString() === path)
   console.log(blog)
   console.log(location)
   return(
     <div className="post__page">
-      <img src={blog.imageUrl} alt="" className="post__img"/>
-      <h1 className="post__title">{blog.title}</h1>
-      <p className="post__desc">{blog.discription}</p>
-      <p className="post__longDesc">{blog.fullDiscription}</p>
+      <img src={blogs.imageUrl} alt="" className="post__img"/>
+      <h1 className="post__title">{blogs.title}</h1>
+      <p className="post__desc">{blogs.discription}</p>
+      <p className="post__longDesc">{blogs.fullDiscription}</p>
     </div>
   )
 }
