d32a0b75c08da575ffef589faa72cf7bd547141f
din1998 <dinislam8803@gmail.com>
2023-03-05 18:10:13 +0600

    create card

--- src/component/Card.js
+++ src/component/Card.js
@@ -7,12 +7,25 @@ export default function Card ({blog}) {
   return (
 
     <div className="card">
-      <Link className="link" to={`/post/${blog._id}`}>
-      <img src={blog.imageUrl} alt="" className="img"/>
-      <h2 className="title">{blog.title}</h2>
-      <p className="desc">{blog.discription}</p>
-      <button className="card__btn">Reade More</button>
-      </Link>
+
+      <div className="card__img">
+        <Link className="link" to={`/post/${blog._id}`}>
+          <img src={blog.imageUrl} alt="" className="img"/>
+        </Link>
+      </div>
+
+      <div className="card__text">
+        <Link className="link" to={`/post/${blog._id}`}>
+        <h2 className="title">{blog.title}</h2>
+        </Link>
+        <p className="desc">{blog.discription}</p>
+        
+        <Link className="link" to={`/post/${blog._id}`}>
+          <button className="card__btn">Reade More</button>
+        </Link>
+        
+      </div>
+     
     </div>
    
   )
--- src/page/Home.js
+++ src/page/Home.js
@@ -7,9 +7,16 @@ export default function Home({blog}) {
   
   return(
     <div className="home__section">
-      {blog.map(blog => (
-        <Card key={blog._id} blog={blog}/>
-      ))}
+      <div className="card__container">
+        {blog.map(blog => (
+          <Card key={blog._id} blog={blog}/>
+        ))}
+      </div>
+      <div className="right__content">
+          <div className="feature__content">
+            <h1>Feature content</h1>
+          </div>
+      </div>
     </div>
   )
 }
