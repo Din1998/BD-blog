ab4f0d2de7f9ba78a94fb28a444722e53a82138b
din1998 <dinislam8803@gmail.com>
2023-03-05 23:39:11 +0600

    add footer component

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import '../css/index.css'
+
+export default function Footer() {
+
+  return(
+    <footer className="footer">
+      <div className="footer__wraper">
+      <span className="logo"><h1>BD Blog</h1></span>
+        <p>© 2022 - 2023 All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
