bde01dce1b7e1aef892928bfca7973e3921011bf
din1998 <dinislam8803@gmail.com>
2023-03-05 17:15:08 +0600

    create navbar component

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faFacebook,faTwitter,faYoutube,faInstagram } from '@fortawesome/free-brands-svg-icons'
 import '../css/index.css'
 
 export default function Navbar ({user}) {
@@ -6,19 +8,41 @@ export default function Navbar ({user}) {
     window.open("http://localhost:8000/auth/logout", "_self");
   };
   return(
-    <div className="navBar">
-      <span className="logo"><Link className="link" to='/'>BD Blog</Link></span>
-      {
-        user ? (
-      <ul className="list">
-        <li className="listItem">
-          <img src={user.photos[0].value} referrerPolicy="no-referrer" alt="prifileImg" className="avatar" />
-        </li>
-        <li className="listItem">{user.displayName}</li>
-        <li className="listItem" onClick={logout}>Logout</li>
-      </ul>
-      ) : (<Link className="link" to="/login">Login</Link>)
-    }
-    </div>
+    <header className="mst__header" role='banner'>
+        <div className="header">
+          <div className="icons">
+            <FontAwesomeIcon className="header_icon" icon={faTwitter} />
+            <FontAwesomeIcon className="header_icon" icon={faFacebook} />
+            <FontAwesomeIcon className="header_icon" icon={faInstagram} />
+            <FontAwesomeIcon className="header_icon" icon={faYoutube} />
+          </div>
+          <div className="logo"><Link className="link" to='/'><h1>BD Blog</h1></Link></div>
+          <div className="login">
+            {
+              user ? (
+            <ul className="list">
+              <li className="listItem">
+                <img src={user.photos[0].value} referrerPolicy="no-referrer" alt="prifileImg" className="avatar" />
+              </li>
+              <li className="listItem">{user.displayName}</li>
+              <li className="listItem" onClick={logout}>Logout</li>
+            </ul>
+            ) : (<Link className="link" to="/login">Login</Link>)
+            }
+          </div>
+        </div>
+        
+        <div className="nav_items">
+            <nav>
+              <a href="#">Home</a>
+              <a href="#">Life Style</a>
+              <a href="#">About</a>
+              <a href="#">Contact</a>
+            </nav>
+        </div>
+        
+
+    </header>
+    
   )
 }
