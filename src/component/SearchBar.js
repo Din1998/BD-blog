9e4f20bd6c1ceabef53ae8fff74d288eb2842e1e
din1998 <dinislam8803@gmail.com>
2023-03-21 00:15:49 +0600

    add serch box

---
+++ src/component/SearchBar.js
@@ -0,0 +1,53 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch} from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
+
+export default function SearchBar({setResults}) {
+
+  const [input,setInput] = useState("")
+
+
+
+
+  // Get data from api
+  const getData =  (value) => {
+     fetch(`https://server2023.vercel.app/api`)
+    .then((response) => response.json())
+    .then((json) => {
+
+      const result = json.filter((blog) => {
+        return(
+          value &&
+          blog &&
+          blog.title &&
+          blog.title.toLowerCase().includes(value)
+        )
+
+      })
+      setResults(result);
+    })
+  }
+
+
+
+
+
+  const handleSubmit = (value) => {
+    setInput(value)
+    getData(value)
+  }
+
+  return(
+    <div className="search__bar">
+     
+      <input 
+        type="text"
+        placeholder="Search Here"     
+        value={input}
+        onChange = {(e) => handleSubmit(e.target.value)}
+      />
+       <FontAwesomeIcon className="header_icon" icon={faSearch} />
+    </div>
+  )
+}
---
+++ src/component/SearchResultList.js
@@ -0,0 +1,14 @@
+
+
+export default function SearchResultList({results}) {
+
+  return(
+    <div className="search__list--wraper">
+       {results.map((result,id) => {
+        return <div className="serch--each__list" key={id}>{result.title}</div>
+       })
+
+       }
+    </div>
+  )
+}
