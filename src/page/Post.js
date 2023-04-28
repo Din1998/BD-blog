0a56212abe9ef811d5d8aa67e372ff369f2bec00
https://github.com/Din1998 <dinislam8803@gmail.com>
2023-04-28 19:03:42 +0600

    add link to dashboard

--- src/page/Post.js
+++ src/page/Post.js
@@ -1,5 +1,9 @@
 import { useLocation } from "react-router-dom";
 import { useEffect,useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faEdit} from '@fortawesome/free-solid-svg-icons'
 import axios from "axios";
 import '../css/index.css';
 import '../App.css';
@@ -39,7 +43,13 @@ export default function Post() {
       <h1 className="post__title">{post.title}</h1>
       <h2 className="post__desc">{post.discription}</h2>
       <p className="post__longDesc">{post.fullDiscription}</p>
-      <p className="current_date">{new Date(post.date).toDateString()}</p>
+      <div className="date__">
+        <p className="current_date">{new Date(post.date).toDateString()}</p>
+        <Link className="link" to='/dashboard'>
+          <FontAwesomeIcon  className="edit__icon" icon={faEdit} />
+        </Link>
+        
+      </div>
       
     </div>
   )
